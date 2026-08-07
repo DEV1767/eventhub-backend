bfcd372d7a5580f8725404c5d93faeef47af719d
shivam chaudhary <shivamchy076@gmail.com>
2026-08-07 10:24:35 +0530

    Added rate limitter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import rateLimit from "express-rate-limit";
 import { connect_db } from "./src/model/db.js";
 
 import authRoutes from "./src/routes/auth.route.js";
@@ -16,6 +16,32 @@ import paymentRoutes from "./src/routes/payment.route.js";
 import { responseFormatterMiddleware } from "./src/middleware/responseFormatter.middleware.js";
 
 const app = express();
+
+//global APi-limiter
+export const apiLimiter = rateLimit({
+    windowMs: 7 * 60 * 1000,
+    limit: 80,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+    message: {
+        success: false,
+        message: "Bro... relax 😂. You have officially scared the server."
+    }
+});
+
+//auth APi-limiter
+export const authLimiter = rateLimit({
+    windowMs: 7 * 60 * 1000,
+    limit: 5,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+    message: {
+        success: false,
+        message: "Too many login attempts. Please try again after 7 minutes."
+    }
+});
 
 // Basic middlewares
 app.use(express.json({ limit: "50mb" }));
@@ -63,18 +89,20 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 
-app.get("/", (req, res) => {
+app.get("/",authLimiter, (req, res) => {
     res.send("Server is running.");
 });
 
 
 // API routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/events", eventRoutes);
-app.use("/api/v1/teams", teamRoutes);
-app.use("/api/v1/schedule", scheduleRoutes);
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/upload", uploadRoutes);
-app.use("/api/v1/payment", paymentRoutes);
+app.use(apiLimiter)
+app.use(authLimiter)
+app.use("/api/v1/auth", authLimiter,authRoutes);
+app.use("/api/v1/events",apiLimiter, eventRoutes);
+app.use("/api/v1/teams", apiLimiter,teamRoutes);
+app.use("/api/v1/schedule",apiLimiter, scheduleRoutes);
+app.use("/api/v1/user", apiLimiter,userRoutes);
+app.use("/api/v1/upload",apiLimiter, uploadRoutes);
+app.use("/api/v1/payment", apiLimiter,paymentRoutes);
 
 export default app;
